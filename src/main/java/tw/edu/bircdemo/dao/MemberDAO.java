6a2afb12a2d144895cb5e54f7d398ab6d8c184d7
luweism <10846031@ntubb.edu.tw>
2019-11-21 19:44:02 +0800

    初始化

---
+++ src/main/java/tw/edu/bircdemo/dao/MemberDAO.java
@@ -0,0 +1,9 @@
+package tw.edu.bircdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+import tw.edu.bircdemo.vo.Member;
+@Repository
+public interface MemberDAO extends BaseDAO<Member> {
+
+}
